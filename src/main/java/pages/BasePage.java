c3efbceb8d38a33392b06114acc20b2ce3704a5e
AlexAksioma <alexmedqwerty@gmail.com>
2025-06-04 21:05:21 +0300

    create login negative

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public abstract class BasePage {
     static WebDriver driver;
@@ -15,4 +16,14 @@ public abstract class BasePage {
             throw new RuntimeException(e);
         }
     }
+
+    public boolean isElementPresent(WebElement element){
+        return element.isDisplayed();
+    }
+
+    public boolean isTextInElementPresent(WebElement element, String text){
+        return element.getText().contains(text);
+    }
+
+
 }
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -2,7 +2,11 @@ package ui_tests;
 
 import dto.User;
 import manager.ApplicationManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -15,5 +19,19 @@ public class LoginTests extends ApplicationManager {
         homePage.clickBtnLoginHeader();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginForm(user);
+        ContactsPage contactsPage = new ContactsPage(getDriver());
+        Assert.assertTrue(contactsPage.isContactsPresent());
+        //System.out.println("after assert");
+    }
+
+    @Test
+    public void loginNegativeTest_wrongPassword(){
+        User user = new User("qa_mail@mail.com", "Qwerty123!ZZZ");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        loginPage.closeAlert();
+        Assert.assertTrue(loginPage.isErrorMessagePresent("Login Failed with code 401"));
     }
 }
