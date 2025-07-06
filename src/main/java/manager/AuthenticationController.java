846bb576e23e9cf50c90faf1b442441f50b19b93
AlexAksioma <alexmedqwerty@gmail.com>
2025-07-06 19:18:33 +0300

    rest lesson 2

--- src/main/java/manager/AuthenticationController.java
+++ src/main/java/manager/AuthenticationController.java
@@ -6,16 +6,21 @@ import io.restassured.response.Response;
 import utils.BaseAPI;
 
 import static io.restassured.RestAssured.given;
+import static utils.PropertiesReader.*;
 
 public class AuthenticationController implements BaseAPI {
 
     public Response requestRegLogin(User user, String url) {
         return given()
-                .contentType(ContentType.JSON)   // Content-type : App/json
+                .baseUri(getProperty("login.properties", "baseUri"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)      // Content-type : App/json
                 .body(user)
                 .when()
-                .post(BASE_URL + url)
+                .post(url)
                 .thenReturn()
                 ;
     }
+
+
 }
