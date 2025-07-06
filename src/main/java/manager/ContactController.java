7fc35d80de705fdd95788bc0617ec9c8b15c98b2
AlexAksioma <alexmedqwerty@gmail.com>
2025-07-06 20:59:45 +0300

    rest lesson 2_1

--- src/main/java/manager/ContactController.java
+++ src/main/java/manager/ContactController.java
@@ -1,12 +1,17 @@
 package manager;
 
+import dto.Contact;
+import dto.TokenDto;
 import dto.User;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.annotations.BeforeSuite;
+import utils.BaseAPI;
 
 import static io.restassured.RestAssured.given;
+import static utils.PropertiesReader.getProperty;
 
-public class ContactController {
+public class ContactController implements BaseAPI {
     /*   given()
             .header("Authorisation", "value")
             .body(user)
@@ -18,5 +23,40 @@ public class ContactController {
        Response
      */
 
+    protected TokenDto tokenDto;
 
+    @BeforeSuite
+    public void login(){
+        User user = new User(getProperty("login.properties", "email"),
+                getProperty("login.properties", "password"));
+        Response response = new AuthenticationController().requestRegLogin(user, LOGIN_URL);
+        //System.out.println(response.getStatusLine());
+        if (response.getStatusCode() == 200) {
+                 tokenDto = response.body().as(TokenDto.class);
+        }else {
+            System.out.println("Smth sent wront --> "+response.getStatusCode());
+        }
+    }
+
+    protected Response addNewContactRequest(Contact contact, TokenDto tokenDto){
+        return given()
+                .body(contact)
+                .baseUri(getProperty("login.properties", "baseUri"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", tokenDto.getToken())
+                .post(ADD_NEW_CONTACT_URL)
+                .thenReturn()
+                ;
+    }
+
+    public Response getAllUserContacts(){
+        return given()
+                .baseUri(getProperty("login.properties", "baseUri"))
+                .accept(ContentType.JSON)
+                .header("Authorization", tokenDto.getToken())
+                .get(ADD_NEW_CONTACT_URL)
+                .thenReturn()
+                ;
+    }
 }
